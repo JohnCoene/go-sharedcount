1603e94a24b1e01e5e2621195a478e96108c5121
JohnCoene <jcoenep@gmail.com>
2019-04-03 22:54:15 +0200

    minor corrections

--- sharedcount/doc.go
+++ sharedcount/doc.go
@@ -1,7 +1,7 @@
 /*
 Package sharedcount provides an interface to the SharedCount API.
-Import with
-	"go-sharedcount/sharedcount"
+Install with
+	go get github.com/JohnCoene/go-sharedcount
 Get a free API key on SharedCount's website then pass API key to various functions;
 always as first argument.
  data := sharedcount.GetURL("myAPIkey", "https://golang.org/")
